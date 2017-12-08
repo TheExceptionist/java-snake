d630b40110ab58fccd3362b8184d97da38e2ffe8
name/name <kennystepney@hotmail.com>
2017-12-07 21:23:51 -0600

    Rebanced some more stuff

--- src/net/thexceptionist/main/GameMain.java
+++ src/net/thexceptionist/main/GameMain.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class GameMain {
 	//Initialize constants such as the grid tile size, width and height of the window
-	public static final int WIDTH = 800, HEIGHT = 600;
+	public static final int WIDTH = 1000, HEIGHT = 600;
 	public static final int TILE_SIZE = 16;
 	//Title of the window
 	public static final String title = "Pong";
